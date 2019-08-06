1bea754b4a8e824af847911474074f017c864158
davinceleecode <wiz22vincent@gmail.com>
2019-08-06 22:24:21 +0800

    adding autoMapper in customerDto

---
+++ Vidly/Vidly/Dtos/CustomerDto.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+using Vidly.Models;
+
+namespace Vidly.Dtos
+{
+    public class CustomerDto
+    {
+
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Name field is required. Please input Name")]
+        [StringLength(255)]
+        public string Name { get; set; }
+
+        public bool IsSubscribedToNewsletter { get; set; }
+
+        public byte MembershipTypeId { get; set; }
+
+
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:MM/dd/yyyy}")]
+        //[Min18YearsIfAMember]
+        public DateTime? Birthdate { get; set; }
+    }
+}
